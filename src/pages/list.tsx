9cb5dc0496b405ded3b60f2cff070483333bf6ad
Danazumi <granzin98@gmail.com>
2024-05-04 18:05:11 +0100

    animation update

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -8,7 +8,7 @@ const ListItem : React.FC<{ item: string; index: number}> = ({ item , index}) =>
     <motion.li
       initial={{ opacity: 0, y:0, }} 
       animate={{opacity: 1, y: -40 }}
-      transition={{ delay: index * 0.1 + 2.6, duration : 0.5 }} // Adjust delay for staggered animation
+      transition={{ delay: index * 0.1 + 2.75, duration : 0.5 }} // Adjust delay for staggered animation
       style={{ listStyleType: 'none' }}
       layout = "position"
     >
@@ -17,15 +17,44 @@ const ListItem : React.FC<{ item: string; index: number}> = ({ item , index}) =>
   );
 };
 
+const Paragraph : React.FC<{ item: string; index: number}> = ({ item , index}) => {
+  return (
+    <motion.li
+      initial={{ opacity: 0, y:0, }} 
+      animate={{opacity: 1, y: -40 }}
+      transition={{ delay: index * 0.02 + 2.6, duration : 0.5 }} // Adjust delay for staggered animation
+      style={{ listStyleType: 'none' }}
+      layout = "position"
+    >
+      {item}
+    </motion.li>
+  );
+};
+
+
+
 const AnimatedList = () => {
   const items = ['search', 'random', 'faves', 'about'];
+  const lines = ['we love music' , 'because it gives light' , 'to our dark world']
 
   return (
-    <ul className = "mt-40 ml-20">
-      {items.map((item, index) => (
-        <ListItem key={index} item={item} index={index} />
-      ))}
+    <div className="font-semibold ">
+      <ul className = "mt-[200px] -ml-[30px] text-[43px] text-black">
+          {lines.map((item, index) => (
+            <Paragraph key={index} item={item} index={index} />
+          ))}
+      </ul>
+      
+      <ul className = "mt-40 -ml-[322px] text-[20px] text-black">
+        {items.map((item, index) => (
+          <ListItem key={index} item={item} index={index} />
+        ))}
     </ul>
+
+
+
+
+    </div>
   );
 };
 
